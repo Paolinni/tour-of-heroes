5059a60814cf285de2f83170d8be3f4f8adc00af
Paolinni <pxv2009@gmail.com>
2016-03-24 23:56:39 -0500

    moved HEROES to seperate file, HeroService created

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,9 @@
 import {Component} from 'angular2/core';
 import {HeroDetailComponent} from './hero-detail.component';
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
+
+constructor(private _heroService: HeroService) {};
 
 @Component({
     selector: 'my-app',
@@ -65,27 +68,15 @@ import {Hero} from './hero';
       border-radius: 4px 0 0 4px;
     }
 `],
-directives: [HeroDetailComponent]
+directives: [HeroDetailComponent],
+providers: [HeroService]
 
 })
 export class AppComponent {
   public title = 'Tour of Heroes';
   public selectedHero: Hero;
-  public heroes = HEROES;
+  public heroes: Hero[];
 
   public onSelect(hero: Hero) { this.selectedHero = hero; }
 }
-
-var HEROES: Hero[] = [
-  { "id": 11, "name": "Mr. Nice" },
-  { "id": 12, "name": "Narco" },
-  { "id": 13, "name": "Bombasto" },
-  { "id": 14, "name": "Celeritas" },
-  { "id": 15, "name": "Magneta" },
-  { "id": 16, "name": "RubberMan" },
-  { "id": 17, "name": "Dynama" },
-  { "id": 18, "name": "Dr IQ" },
-  { "id": 19, "name": "Magma" },
-  { "id": 20, "name": "Tornado" }
-];
 
