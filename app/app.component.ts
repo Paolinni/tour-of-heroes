4f52ee65da21402377343fd6e7c9711f9258bb37
Paolinni <pxv2009@gmail.com>
2016-03-26 02:35:25 -0500

    Dashboard and Heroes nav links

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,40 @@
 import {Component} from 'angular2/core';
 import {HeroService} from './hero.service';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
+// The AppComponent is now attached to a router and displaying routed views. For this reason and to distinguish it from other kinds of components, we call this type of component a Router Component.
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
-  `,
-  directives: [HeroesComponent],
+  <h1>{{title}}</h1>
+  <nav>
+  <a [routerLink]="['Dashboard']">Dashboard</a>
+  <a [routerLink]="['Heroes']">Heroes</a>
+  </nav>
+  <router-outlet></router-outlet>
+`,
+  directives: [ROUTER_DIRECTIVES],
   providers: [
+    ROUTER_PROVIDERS,
     HeroService
   ]
 })
+@RouteConfig([
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
 export class AppComponent {
   title = 'Tour of Heroes';
 }
